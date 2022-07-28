aa8973d0eb92488467c3d3ce6449e3b504eb91f6
Roderick Fayson <roderickfayson@Rodericks-MacBook-Air.local>
2022-07-28 15:48:34 -0400

    more

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,25 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+// setup routes...
+app.get('/test',(req, res) =>{
+    res.send('Wow, it actually worked');
+});
+
+app.get('/another-one',(req, res) =>{
+    res.send('This one works, too!');
+});
+
+exports.api = functions.https.onRequest(app)
+
 
 exports.helloThere = functions.https.onRequest((req, res) => {
     res.send('Hello there Firebase Cloud user!');
-})
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
